47cb02c97b893085a597af36e1151a38f0b0cbfc
Stuart P. Bentley <stuart@testtrack4.com>
2013-03-09 16:38:51 -0500

    New throner behaviors (like making circle position a parameter on creation)

--- throner.js
+++ throner.js
@@ -4,11 +4,31 @@
 var count = 0;
 var twopi = Math.PI * 2;
 
-function addCircle() {
+function addCircle(x,y) {
   var circle = document.createElementNS("http://www.w3.org/2000/svg",'circle');
   var text = document.createElementNS("http://www.w3.org/2000/svg",'text');
   var g = document.createElementNS("http://www.w3.org/2000/svg",'g');
 
+  circle.setAttribute('class','none');
+  circle.setAttribute('r','.35in');
+  circle.setAttribute('cx',0);
+  circle.setAttribute('cy',0);
+
+  text.setAttribute('x',0);
+  text.setAttribute('y',0.075+'in');
+
+  g.setAttribute('data-draggable','');
+  g.setAttribute('transform','translate('+x+' '+y+')');
+
+  g.appendChild(circle);
+  g.appendChild(text);
+
+  document.getElementById('viewport').appendChild(g);
+  ++count;
+  return {g:g, circle: circle, text: text};
+}
+
+function addSetupLine () {
   //determine location
   var ring = (Math.floor(count/6)+1);
   var distance = (Math.sqrt(ring))*(Math.sqrt(2));
@@ -19,26 +39,8 @@ function addCircle() {
   var x = 5+distance*Math.cos(angle);
   var y = 4+distance*Math.sin(angle);
 
-  circle.setAttribute('class','none');
-  circle.setAttribute('r','.35in');
-  circle.setAttribute('cx',x+'in');
-  circle.setAttribute('cy',y+'in');
+  var cg = addCircle(x,y);
 
-  text.setAttribute('x',x+'in');
-  text.setAttribute('y',(y+0.075)+'in');
-
-  g.setAttribute('data-draggable','');
-
-  g.appendChild(circle);
-  g.appendChild(text);
-
-  document.getElementById('viewport').appendChild(g);
-  ++count;
-  return {circle: circle, text: text};
-}
-
-function addSetupLine () {
-  var cg = addCircle();
   var radopts = ["mgr","ffr","exile","none"];
   var labels = {
     mgr: "Manager",
@@ -93,9 +95,22 @@ function addSetupLine () {
 }
 
 function setupPage(){
-  addSetupLine();
   setupSVGPan(document.getElementById("printout"),
     document.getElementById("viewport"));
+  document.getElementById("partsbox")
+    .addEventListener('mousedown', summonCircle, false);
+}
+
+function summonCircle(evt){
+  //use a function from SVGPan
+  var p = getEventPoint(evt);
+  //TODO: X and Y need to be transformed by the SVG's current zoom/pan -
+  //currently they just put the circle at the Parts box's screen position.
+  var c = addCircle(p.x,p.y);
+  console.log(c.g);
+  //hook into variables in SVGPan
+  state = "drag";
+  stateTarget = c.g;
 }
 
 function togglePanel(){
